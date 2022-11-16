c4a1c55e25ffa7055159322e5a819aa182adf9ca
Brandon Segelke <bbsegelke@gmail.com>
2022-11-16 05:48:40 +0000

    finsihed matchy project

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,17 +13,86 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+//  1. Open up the file `functions.js` in your editor.
+
+//  2. Implement a function declaration called `search` that:
+function search(animalArray, animalName){
+    //loop through animal array 
+    for(var i = 0; i < animalArray.length; i++){
+    //if array[I] == string return that animal
+    if(animalArray[i].name.toLowerCase() == animalName.toLowerCase() ){
+     
+        return animalArray[i]
+    }else {
+        
+        
+    
+    
+    }
+    
+    }
+    //else return null
+    return null;
+    }
+//    - Takes a paramater representing an Array of `animals`.
+//    - Takes a paramater representing a String, the name of an animal on which to perform a search.
+//    - Looks through the `animals` Array, and returns the animal's Object if an animal with that name exists.
+//    - Returns `null` if no animal with that name exists
+
+//  3. Use the search bar at the top of the page to make sure your function works.
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// 1. Write a function declaration called `replace` with a signature of `replace(animals, name, replacement) { //... } ` that:
+//     - Takes 3 parameters, an Array of animals, a String representing  the name of an animal on which to perform a search, and an Object that represents the replacement animal.
+//     - If an animal with that name exists within the `animals` Array, replace it's entire Object with the replacement Object.
+//     - Otherwise do nothing.
+//   2. Preview the `index.html` page to test it on the website.
 
 
+// write a function replace that takes in the animals array, a string that represents a name, and a replacement
+function replace(animals, name, replacement){
+//loop throgh the animals array
+for(var i = 0; i < animals.length; i++){
+//if animals[i].name == name then animals[i].name = replacement
+if (animals[i].name === name){
+    animals[i] = replacement
+}
+return animals
+
+}
+
+
+}
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+
+// 1. Write a function declaration called `remove` with a signature of `remove(animals, name)` that:
+// - Takes 2 parameters, an Array of animals, and a name of an animal on which to perform a search.
+// - If an animal with that name exists within the `animals` Array, remove it.
+// 2. Test that it works on the website.
+
+//function called remove that takes in animals, and a name
+function remove(animals, name){
+//loop through the array to get an index number
+for(var i = 0; i < animals.length; i++){
+//if animal[i].name === name
+if( animals[i].name === name){
+animals.splice([i],1)
+
+}
+//remove it from array
+
+}
+
+}
+
+
+
 
 
 
@@ -31,6 +100,32 @@
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// 1. Write a function declaration called `add` with a signature of `add(animals, animal) { //... }` that:
+//     - Takes 2 parameter, an Array of animals, and an Object representing a new animal to be added.
+
+//     - Checks that the animal Object has a `name` property with a length > 0.
+
+//     - Checks that the animal Object has a `species` property with a length > 0.
+//     - Has a **unique** name, meaning no other animals have that name.
+//     - Adds this new Object to the `animals` Array, **only** if all the other conditions pass.
+//     - Make sure it works.
+
+
+//make function called add that takes animals array, and an animal object as parameters
+function add(animals, animalObj){
+    let itemsExist = false;
+// for loop to get index values
+for(var i = 0; i < animals.length; i++){
+//if animalobj has a .length greater than 0 AND species proeprty greater than 0 AND no other animals have its name
+if(animals[i].name === animalObj.name){
+    itemsExist = true;
+} 
+//add the new obejct to the animals array
+}
+if(itemsExist === false && animalObj.name.length > 0 && animalObj.species.length > 0) {
+    animals.push(animalObj);
+}
+}
 
 
 /**
