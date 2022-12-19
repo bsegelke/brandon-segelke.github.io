6ede61095656ec8bd493864d455846275893fe52
Brandon Segelke <bbsegelke@gmail.com>
2022-12-19 23:19:11 +0000

    alldone

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -25,17 +25,46 @@ function loop(start, test, update, body) {
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
-
-}
+function every(array, test){
+  for(let i = 0; i < array.length; i++){
+    if(test(array[i]) !== true){
+      return false;
+    }
+  }
+  return true;
+  
+  }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
+function dominantDirection(string){
+  //create arrays t ostore ltr and rtl
+let ltr = [];
+let rtl = [];
+for(let i = 0; i < string.length; i++){
+let script = characterScript(string.charCodeAt(i));
+if( script !== null){
+  if(script.direction === 'ltr'){
+    ltr.push(script);
+  }else if (script.direction === 'rtl'){
+    rtl.push(script);
+  }
+}
+}
+if (ltr.length > rtl.length){
+  return 'ltr'
+}else {
+  return 'rtl'
 
 }
+};
+
+
+
+
+
 
 // /////////////////////////////////////////////////////////////////////////////
 //  //////////////////////////////////////////////////////
